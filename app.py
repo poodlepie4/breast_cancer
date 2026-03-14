d8b4e0eb2b5124a2bfe94981f7b8e2f4ece9c780
poodlepie4 <160336978+poodlepie4@users.noreply.github.com>
2026-03-14 20:03:18 +0530

    app.py

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ def predict():
 
     return render_template("index.html", prediction=result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
